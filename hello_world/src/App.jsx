c08507fb7a177ddaee4e9f3f378136eefa2829ca
ashish-abir <ashish.ranjan@abirnetworks.com>
2025-03-14 15:26:15 +0530

    added

---
+++ hello_world/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div
+            style={{
+                minHeight: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                padding: "15px",
+            }}
+        >
+            Hello World! This is a2rp: an Ashish Ranjan presentation
+        </div>
+    );
+};
+
+export default App;
