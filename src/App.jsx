2e4db28f84bea18c1a6acff73cdb3545434c02b4
ashish-abir <ashish.ranjan@abirnetworks.com>
2025-03-14 21:39:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const App = () => {
-    return <Styled.Wrapper>App</Styled.Wrapper>;
+    return <Styled.Wrapper>React App</Styled.Wrapper>;
 };
 
 export default App;
